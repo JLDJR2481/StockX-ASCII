85c51d661888bcc5ea88dfd3f1cab5ae03964fae
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-01-30 19:37:33 +0100

    Primera historia de Usuario conseguida

---
+++ stockx/src/main/java/edu/craptocraft/stockx/item/Sneaker.java
@@ -0,0 +1,75 @@
+package edu.craptocraft.stockx.item;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Sneaker implements Item {
+
+    private String style;
+    private final String name;
+    private int sale;
+    private int ask;
+    private int bid;
+    private List<Offer> offers = new ArrayList<Offer>();
+
+    public Sneaker(String style, String name) {
+        this.style = style;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder output = new StringBuilder();
+        output.append("Style: ").append(style).append("\n");
+        output.append("Name: ").append(name);
+
+        return output.toString();
+
+    }
+
+    @Override
+    public List<Offer> offers() {
+        return this.offers;
+
+    }
+
+    @Override
+    public void add(Offer offer) {
+        offers().add(offer);
+
+    }
+
+    @Override
+    public void setAsk(int ask) {
+        this.ask = ask;
+    }
+
+    @Override
+    public int getAsk() {
+        return this.ask;
+    }
+
+    @Override
+    public void setBid(int bid) {
+        this.bid = bid;
+
+    }
+
+    @Override
+    public int getBid() {
+        return this.bid;
+
+    }
+
+    @Override
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public int getSale() {
+        return this.sale;
+    }
+
+}
